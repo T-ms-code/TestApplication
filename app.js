3d06c4baf94880c07f08c0c11edf339aa9ac5d80
Mihai Telu <mihai.telu@intern.essensys.ro>
2026-09-14 12:43:57 +0300

    Delete the route for power

--- app.js
+++ app.js
@@ -31,17 +31,4 @@ app.get("/divide", (req, res) => {
   res.json({ operation: "divide", result: a / b });
 });
 
-app.get("/power", (req, res) => {
-  const base = parseFloat(req.query.base) || 0;
-  const exponent = parseFloat(req.query.exponent) || 0;
-
-  if (exponent > 100) {
-    return res
-      .status(400)
-      .json({ error: "The maximum allowed exponent is 100." });
-  }
-
-  res.json({ operation: "power", result: Math.pow(base, exponent) });
-});
-
 module.exports = { app, port };
